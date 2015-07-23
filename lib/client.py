13d3da795a698b6ffdde89dd9b7c699124e44580
Muneeb Ali <muneeb@ali.vc>
2015-07-23 17:58:45 -0400

    added config file plus pep8 updates
    added config file plus pep8 updates

--- lib/client.py
+++ lib/client.py
@@ -1,45 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+    API Client
+    ~~~~~
+    :copyright: (c) 2015 by Halfmoon Labs
+    :license: MIT, see LICENSE for more details.
+"""
+
 import json
 import requests
 
+from .config import ONENAME_API_ID, ONENAME_API_SECRET
+
+
 class Client:
 
-	def __init__(self, key_id, key_secret):
-		self.key_id = key_id
-		self.key_secret = key_secret
-		self.base_url = "https://api.onename.com/v1"
+    def __init__(self, api_id=ONENAME_API_ID, api_secret=ONENAME_API_SECRET):
 
-	def _get_request(self, url):
-		response = requests.get(url, auth=(self.key_id, self.key_secret), verify=False)
-		return json.loads(response.text)
+        self.api_id = api_id
+        self.api_secret = api_secret
+        self.base_url = "https://api.onename.com/v1"
 
-	def _post_request(self, url, payload):
-		response = requests.post(url, data=payload, auth=(self.key_id, self.key_secret), verify=False)
-		return json.loads(response.text)
+    def _get_request(self, url):
 
+        response = requests.get(url, auth=(self.api_id, self.api_secret),
+                                verify=False)
 
-	def get_user(self, username):
-		url = self.base_url + "/users/" +  username
-		return self._get_request(url)
+        return json.loads(response.text)
 
-	def get_search(self, query):
-		url = self.base_url + "/search?query=" + query
-		return self._get_request(url)
+    def _post_request(self, url, payload):
 
-	def get_stats(self):
-		url = self.base_url + "/users"
-		return self._get_request(url)
+        response = requests.post(url, data=payload,
+                                 auth=(self.api_id, self.api_secret),
+                                 verify=False)
 
-	def get_address(self, address):
-		url = self.base_url + "/addresses/" + address
-		return self._get_request(url)
+        return json.loads(response.text)
 
-	def register_user(self, payload):
-		url = self.base_url + "/users"
-		return self._post_request(url, payload)
+    def get_user(self, username):
 
-	def broadcast_transaction(self, payload):
-		url = self.base_url +  "/transactions"
-		return self._post_request(url, payload)
+        url = self.base_url + "/users/" + username
 
+        return self._get_request(url)
 
+    def get_search(self, query):
 
+        url = self.base_url + "/search?query=" + query
+
+        return self._get_request(url)
+
+    def get_stats(self):
+
+        url = self.base_url + "/users"
+
+        return self._get_request(url)
+
+    def get_address(self, address):
+
+        url = self.base_url + "/addresses/" + address
+
+        return self._get_request(url)
+
+    def register_user(self, payload):
+
+        url = self.base_url + "/users"
+
+        return self._post_request(url, payload)
+
+    def broadcast_transaction(self, payload):
+
+        url = self.base_url + "/transactions"
+
+        return self._post_request(url, payload)
