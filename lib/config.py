65a5030e95bf0b63e4edbd118b7ab4907fa84453
Muneeb Ali <muneeb@ali.vc>
2015-07-24 12:23:53 -0400

    tested the post to registration endpoint and small fixes to it

--- lib/config.py
+++ lib/config.py
@@ -13,3 +13,9 @@ try:
     ONENAME_API_SECRET = os.environ['ONENAME_API_SECRET']
 except:
     ONENAME_API_ID = ONENAME_API_SECRET = None
+
+
+try:
+    from config_local import ONENAME_BASE_URL
+except:
+    ONENAME_BASE_URL = "https://api.onename.com/v1"
